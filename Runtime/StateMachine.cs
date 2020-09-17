8e9a9d6577e89619ff9b51eefc4b7790dc253149
shogo ogawa <kuro.1203.kuro@gmail.com>
2020-09-18 02:37:07 +0900

    update:statemachine改修

---
+++ Runtime/StateMachine.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class StateMachine<TTrigger>
+{
+    private StateObject stateObject = null;
+    private Dictionary<TTrigger, Type> stateTypeDictionary = null;
+    private TTrigger requestState = default;
+    private bool isRequestState = false;
+    private bool canUpdate = true;
+
+    /// <summary>
+    /// 現在のState
+    /// </summary>
+    /// <value></value>
+    public TTrigger CurrentState { get; private set; } = default;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public StateMachine()
+    {
+        CurrentState = default;
+        stateObject = null;
+        stateTypeDictionary = new Dictionary<TTrigger, Type>();
+        canUpdate = false;
+        isRequestState = false;
+    }
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    public void Initialize()
+    {
+        canUpdate = true;
+        CurrentState = default;
+        requestState = default;
+        stateObject = null;
+        isRequestState = false;
+    }
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    public void Update()
+    {
+        if (stateObject.IsBegan == false)
+        {
+            stateObject.OnBegin();
+        }
+
+        if (stateObject.IsStart == false)
+        {
+            stateObject.OnStart();
+        }
+
+        stateObject.OnUpdate();
+
+        if (isRequestState == true)
+        {
+            CurrentState = requestState;
+            stateObject.OnStop();
+            stateObject.OnEnd();
+            stateObject = Create(CurrentState);
+            isRequestState = false;
+        }
+    }
+
+    /// <summary>
+    /// 終了
+    /// </summary>
+    public void Terminate()
+    {
+        canUpdate = false;
+        stateObject = null;
+    }
+
+    /// <summary>
+    /// Initialize前に呼ぶ
+    /// StateとStateObjectのTypeを追加
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="type"></param>
+    public void AddStateType(TTrigger value, Type type)
+    {
+        stateTypeDictionary.Add(value, type);
+    }
+
+    /// <summary>
+    /// Stateの削除
+    /// </summary>
+    /// <param name="value"></param>
+    public void RemoveState(TTrigger value)
+    {
+        stateTypeDictionary.Remove(value);
+    }
+
+    /// <summary>
+    /// Stateの変更をリクエスト
+    /// </summary>
+    /// <param name="value"></param>
+    public void RequestSetState(TTrigger value)
+    {
+        if (isRequestState == true)
+        {
+#if UNITY_ENGINE
+            Debug.Warning("すでにStateの切り替えをリクエストしています。");
+#endif
+            return;
+        }
+
+        requestState = value;
+    }
+
+    /// <summary>
+    /// StateObjectの生成
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    private StateObject Create(TTrigger value)
+    {
+        return Activator.CreateInstance(stateTypeDictionary[value]) as StateObject;
+    }
+}
---
+++ Runtime/StateObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class StateObject
+{
+    public bool IsBegan { get; private set; } = false;
+    public bool IsStart { get; private set; } = false;
+
+    public virtual void OnBegin()
+    {
+        IsBegan = true;
+    }
+
+    public virtual void OnStart()
+    {
+        IsStart = true;
+    }
+
+    public virtual void OnUpdate()
+    {
+
+    }
+
+    public virtual void OnStop()
+    {
+        IsStart = false;
+    }
+
+    public virtual void OnEnd()
+    {
+        IsBegan = false;
+    }
+}
+
