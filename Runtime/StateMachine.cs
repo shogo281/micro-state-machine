ae924e1c343bcb0dc2dcd7ef5420ab971403ff2a
shogo ogawa <kuro.1203.kuro@gmail.com>
2020-09-19 19:39:30 +0900

    update:StateMachine実装

--- Runtime/StateMachine.cs
+++ Runtime/StateMachine.cs
@@ -3,124 +3,247 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
-public class StateMachine<TTrigger>
+namespace MicroStateMachine
 {
-    private StateObject stateObject = null;
-    private Dictionary<TTrigger, Type> stateTypeDictionary = null;
-    private TTrigger requestState = default;
-    private bool isRequestState = false;
-    private bool canUpdate = true;
-
-    /// <summary>
-    /// 現在のState
-    /// </summary>
-    /// <value></value>
-    public TTrigger CurrentState { get; private set; } = default;
-
-    /// <summary>
-    /// コンストラクタ
-    /// </summary>
-    public StateMachine()
+    public class StateMachine<TTrigger, TContext> : IDisposable where TContext : class
     {
-        CurrentState = default;
-        stateObject = null;
-        stateTypeDictionary = new Dictionary<TTrigger, Type>();
-        canUpdate = false;
-        isRequestState = false;
-    }
+        /// <summary>
+        /// Stateが切り替わったときのイベント
+        /// </summary>
+        public event Action OnChangeState = null;
+        private readonly TContext context = null;
+        private StateObject<TTrigger, TContext> stateObject = null;
+        private Dictionary<TTrigger, StateObject<TTrigger, TContext>> triggerWithStateObjectDictionary = null;
+        private TTrigger requestState = default;
+        private bool isRequestState = false;
+        private bool isDisposed = false;
 
-    /// <summary>
-    /// 初期化
-    /// </summary>
-    public void Initialize()
-    {
-        canUpdate = true;
-        CurrentState = default;
-        requestState = default;
-        stateObject = null;
-        isRequestState = false;
-    }
+        /// <summary>
+        /// 現在のState
+        /// </summary>
+        /// <value></value>
+        public TTrigger CurrentTrigger { get; private set; } = default;
 
-    /// <summary>
-    /// 更新
-    /// </summary>
-    public void Update()
-    {
-        if (stateObject.IsBegan == false)
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public StateMachine()
         {
-            stateObject.OnBegin();
+            CurrentTrigger = default;
+            stateObject = null;
+            triggerWithStateObjectDictionary = new Dictionary<TTrigger, StateObject<TTrigger, TContext>>();
+            isRequestState = false;
+            OnChangeState = null;
+            isDisposed = false;
         }
 
-        if (stateObject.IsStart == false)
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="context"></param>
+        public StateMachine(TContext context)
         {
-            stateObject.OnStart();
+            CurrentTrigger = default;
+            stateObject = null;
+            triggerWithStateObjectDictionary = new Dictionary<TTrigger, StateObject<TTrigger, TContext>>();
+            isRequestState = false;
+            OnChangeState = null;
+            this.context = context;
+            isDisposed = false;
         }
 
-        stateObject.OnUpdate();
-
-        if (isRequestState == true)
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        public void Initialize()
         {
-            CurrentState = requestState;
-            stateObject.OnStop();
-            stateObject.OnEnd();
-            stateObject = Create(CurrentState);
+            CurrentTrigger = default;
+            requestState = default;
+            stateObject = null;
             isRequestState = false;
         }
-    }
 
-    /// <summary>
-    /// 終了
-    /// </summary>
-    public void Terminate()
-    {
-        canUpdate = false;
-        stateObject = null;
-    }
-
-    /// <summary>
-    /// Initialize前に呼ぶ
-    /// StateとStateObjectのTypeを追加
-    /// </summary>
-    /// <param name="value"></param>
-    /// <param name="type"></param>
-    public void AddStateType(TTrigger value, Type type)
-    {
-        stateTypeDictionary.Add(value, type);
-    }
-
-    /// <summary>
-    /// Stateの削除
-    /// </summary>
-    /// <param name="value"></param>
-    public void RemoveState(TTrigger value)
-    {
-        stateTypeDictionary.Remove(value);
-    }
-
-    /// <summary>
-    /// Stateの変更をリクエスト
-    /// </summary>
-    /// <param name="value"></param>
-    public void RequestSetState(TTrigger value)
-    {
-        if (isRequestState == true)
+        /// <summary>
+        /// 更新
+        /// </summary>
+        public void Update()
         {
+#if UNITY_EDITOR
+            if (isDisposed == true)
+            {
+                Debug.LogWarning("すでにDisposeが呼ばれている。");
+                return;
+            }
+#endif
+            if (stateObject.IsBegan == false)
+            {
+                stateObject.OnBegin();
+            }
+
+            if (stateObject.IsStart == false)
+            {
+                stateObject.OnStart();
+            }
+
+            stateObject.OnUpdate();
+
+            if (isRequestState == true)
+            {
+                CurrentTrigger = requestState;
+                stateObject.OnStop();
+                stateObject = GetStateObject(CurrentTrigger);
+                isRequestState = false;
+
+                if (OnChangeState != null)
+                {
+                    OnChangeState.Invoke();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (var pair in triggerWithStateObjectDictionary)
+            {
+                if (pair.Value == null)
+                {
+                    continue;
+                }
+
+                var instance = pair.Value;
+
+                if (instance == null)
+                {
+                    continue;
+                }
+
+                instance.Dispose();
+            }
+            stateObject = null;
+            OnChangeState = null;
+            CurrentTrigger = default;
+            isRequestState = false;
+            triggerWithStateObjectDictionary = null;
+            isDisposed = true;
+        }
+
+        /// <summary>
+        /// トリガーとインスタンスを紐付ける。
+        /// 呼び出し時にインスタンスを生成する
+        /// </summary>
+        /// <param name="trigger"></param>
+        /// <param name="type"></param>
+        public void AddTriggerAndCreateStateObjectInstance(TTrigger trigger, Type type)
+        {
+            AddTriggerAndInstance(trigger, CreateInstance(type));
+        }
+
+        /// <summary>
+        /// トリガーとインスタンスを紐付ける。
+        /// </summary>
+        /// <param name="trigger"></param>
+        /// <param name="stateObject"></param>
+        public void AddTriggerAndInstance(TTrigger trigger, StateObject<TTrigger, TContext> stateObject)
+        {
+            if (triggerWithStateObjectDictionary.ContainsKey(trigger) == true)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("すでに追加しているトリガーです。");
+#endif
+                return;
+            }
+
+            triggerWithStateObjectDictionary.Add(trigger, stateObject);
+        }
+
+        /// <summary>
+        /// トリガーとインスタンスを削除する
+        /// </summary>
+        /// <param name="trigger"></param>
+        /// <returns></returns>
+        public bool RemoveTriggerAndInstance(TTrigger trigger)
+        {
+            var isContains = triggerWithStateObjectDictionary.ContainsKey(trigger) == false;
+
+            if (isContains == false)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("追加されていないトリガーを削除しようとしました。");
+#endif
+                return isContains;
+            }
+
+            var instance = triggerWithStateObjectDictionary[trigger];
+
+            if (instance != null)
+            {
+                if (instance.IsStart == true)
+                {
+                    instance.OnStop();
+                }
+
+                if (instance.IsBegan == true)
+                {
+                    instance.OnEnd();
+                }
+            }
+
+            return triggerWithStateObjectDictionary.Remove(trigger);
+        }
+
+        /// <summary>
+        /// Stateの変更をリクエスト
+        /// </summary>
+        /// <param name="value"></param>
+        public void RequestSetState(TTrigger value)
+        {
+            if (isRequestState == true)
+            {
 #if UNITY_ENGINE
             Debug.Warning("すでにStateの切り替えをリクエストしています。");
 #endif
-            return;
+                return;
+            }
+
+            requestState = value;
         }
 
-        requestState = value;
-    }
+        /// <summary>
+        /// StateObjectを生成する
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private StateObject<TTrigger, TContext> CreateInstance(Type type)
+        {
+            var instance = Activator.CreateInstance(type, this, context) as StateObject<TTrigger, TContext>;
 
-    /// <summary>
-    /// StateObjectの生成
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    private StateObject Create(TTrigger value)
-    {
-        return Activator.CreateInstance(stateTypeDictionary[value]) as StateObject;
+#if UNITY_EDITOR
+            if (instance == null)
+            {
+                Debug.LogError(type.ToString() + "は無効。");
+            }
+#endif
+            return instance;
+        }
+
+        private StateObject<TTrigger, TContext> GetStateObject(TTrigger trigger)
+        {
+#if UNITY_EDITOR
+            if (triggerWithStateObjectDictionary.ContainsKey(trigger) == false)
+            {
+                Debug.LogError("追加していないトリガー。");
+            }
+#endif
+
+            var instance = triggerWithStateObjectDictionary[trigger];
+
+#if UNITY_EDITOR
+            if (instance == null)
+            {
+                Debug.LogError(trigger.ToString() + "に紐付いているインスタンスはnullです。");
+            }
+#endif
+            return instance;
+        }
     }
 }
